a2b96696582b25e571f8e2d015c5f8ae810f94a3
paul7299 <paul7299@stud.kea.dk>
2023-11-22 13:58:06 +0100

    updating system msg to contain instructions and full nutrional values and more

--- src/main/java/dat3/openai_demo/api/MealMentorController.java
+++ src/main/java/dat3/openai_demo/api/MealMentorController.java
@@ -17,40 +17,48 @@ public class MealMentorController {
           "When prompted, you will produce a JSON object detailing a meal plan that includes meal titles, ingredients with quantities, " +
           "and approximate calorie count for the meal. "
                   +
+                  "Ingredients should be presented as a simple string, for example 3 eggs or 2 tbsp salt or 200g flour"
+                  +
                   "In the \"Description\" section, provide a short explanation for the nutritional value of the dish, " +
                   "and why this specific meal have been picked for the user, " +
-                  "in relation the user's weight, activity level, preferences, allergies and the users goals." +
-                  "Be sure to mention that the dish do not contain what the user is allergic to." +
-                  ""
+                  "in relation the user's weight, activity level, preferences, allergies and the users goals."
                   +
-                  "The JSON object must follow this structure for each day of the week:\n" +
+                  "Be sure to mention that the dish do not contain what the user is allergic to."
+                  +
+                  "mealType is breakfast, lunch or dinner"
+                  +
+
                   "\n" +
                   "{\n" +
-                  "  \"Day\": {\n" +
-                  "    \"MealType\": {\n" +
-                  "      \"Meal\": \"Name of the Meal\",\n" +
+                  "    \"mealType\": {\n" +
+                  "      \"mealTitle\": \"Name of the Meal\",\n" +
                   "      \"Ingredients\": {\n" +
-                  "        \"Ingredient1\": \"Quantity\",\n" +
-                  "        \"Ingredient2\": \"Quantity\",\n" +
+                  "        \"Amount of Ingredient1 and Ingredient1\",\n" +
+                  "        \"Amount of Ingredient2 and Ingredient2\",\n" +
                   "        ...\n" +
                   "      },\n" +
                   "      \"Calories\": \"Calorie count\"\n" +
+                  "      \"Protein\": \"Protein count\"\n" +
+                  "      \"Carbohydrates\": \"Carbohydrate count\"\n" +
+                  "      \"Fat\": \"Fat count\"\n" +
                   "      \"Description\": \"Description\"\n" +
+                  "      \"Instructions\": \"Instruction on how to prepare the meal\"\n" +
                   "    },\n" +
                   "    ...\n" +
                   "  },\n" +
                   "  ...\n" +
-                  "}\n" +
                   "\n" +
-          "Each ingredient's quantity should be listed in metric units, and the units should be included in the same JSON object as the quantity. " +
-          "Include meals for Breakfast, Lunch, and Dinner, depending on the user input. If the user's request lacks details for meal plan creation, " +
-          "the API should respond with a JSON object asking for the necessary information. The API's responses should consistently follow this format " +
-          "and should contain only the JSON-formatted meal plan, not any regular text, and no followup questions.\n";
+          "Each ingredient's quantity should be listed in metric units or amount." +
+          "Include meals for Breakfast, Lunch, and/or Dinner, depending on the user input. " +
+                  "If the user's request lacks details for meal plan creation, " +
+                  "the API should respond with a JSON object asking for the necessary information. " +
+                  "The API's responses should consistently follow this format " +
+                  "and should contain only the JSON-formatted meal plan, not any regular text, and no followup questions.\n" +
+                  "The response should only contain meals for 1 day\n";
 
   public MealMentorController(OpenAiService service) {
     this.service = service;
   }
-
 
 
   @PostMapping()
@@ -61,7 +69,9 @@ public class MealMentorController {
               + userPromptResponse.getSex() +
               "and " + "i workout" + userPromptResponse.getWorkoutsPerWeek() + "times a week " +
               "The recipes should include" + userPromptResponse.getMealChecklist() + "and must not include " + userPromptResponse.getPreferences()
-              + ". My goals are" + userPromptResponse.getGoals() + " i want the recipe made for " + userPromptResponse.getAmountOfDays() + "number of days";
+              + ". My goals are" + userPromptResponse.getGoals()
+
+               + " i want the recipe made for " /* + userPromptResponse.getAmountOfDays() + "number of days" */ ;
 
       return service.makeRequest(userPrompt, SYSTEM_MESSAGE);
 
