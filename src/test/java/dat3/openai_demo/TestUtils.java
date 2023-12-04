88d482985ecf8c96d751a06718f32f2534520729
mathiasfri <55091986+mathiasfri@users.noreply.github.com>
2023-12-04 15:47:31 +0100

    UserService tests done

--- src/test/java/dat3/openai_demo/TestUtils.java
+++ src/test/java/dat3/openai_demo/TestUtils.java
@@ -1,17 +1,28 @@
 package dat3.openai_demo;
 
+import dat3.openai_demo.entity.Allergy;
 import dat3.openai_demo.entity.User;
 import dat3.openai_demo.repository.AllergyRepository;
 import dat3.openai_demo.repository.UserRepository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TestUtils {
-    public static void setupTestUsers(UserRepository userRepository){
+    public static void setupTestUsers(UserRepository userRepository, AllergyRepository allergyRepository){
+        allergyRepository.deleteAll();
+        Allergy peanutAllergy = new Allergy("Peanuts");
+        allergyRepository.save(peanutAllergy);
+        List<Allergy> allergies = new ArrayList<>();
+        allergies.add(peanutAllergy);
+
         userRepository.deleteAll();
         String passwordUsedByAll = "password";
+
         User user1 = new User("testuser1", passwordUsedByAll, "testuser1@test.dk", 80,
-                180, 80, 25, null, "Male", "Moderate", "Gain muscle");
+                180, 80, 25, allergies, "Male", "Moderate", "Gain muscle");
         User user2 = new User("testuser2", passwordUsedByAll, "testuser2@test.dk", 90,
-                200, 50, 0, null, "Male", "No excercise", "Lose weight");
+                200, 50, 0, allergies, "Male", "No excercise", "Lose weight");
 
         userRepository.save(user1);
         userRepository.save(user2);
--- src/test/java/dat3/openai_demo/service/UserServiceTest.java
+++ src/test/java/dat3/openai_demo/service/UserServiceTest.java
@@ -1,6 +1,9 @@
 package dat3.openai_demo.service;
 
 import dat3.openai_demo.TestUtils;
+import dat3.openai_demo.dtos.UserRequest;
+import dat3.openai_demo.dtos.UserResponse;
+import dat3.openai_demo.entity.User;
 import dat3.openai_demo.repository.AllergyRepository;
 import dat3.openai_demo.repository.UserRepository;
 import dat3.openai_demo.service.UserService;
@@ -9,11 +12,16 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 @DataJpaTest
 class UserServiceTest {
-    @Autowired
     UserService userService;
+    @Autowired
     UserRepository userRepository;
+    @Autowired
     AllergyRepository allergyRepository;
 
     private boolean dataInitialized = false;
@@ -23,18 +31,41 @@ class UserServiceTest {
         userService = new UserService(userRepository, allergyRepository);
         if(!dataInitialized) {
             userRepository.deleteAll();
-            TestUtils.setupTestUsers(userRepository);
+            TestUtils.setupTestUsers(userRepository, allergyRepository);
             dataInitialized = true;
         }
     }
 
     @Test
     void getUser() {
+        UserResponse user = userService.getUser("testuser1");
+        assertEquals(1, user.getAllergies().size());
+    }
 
+    @Test
+    void getUsers(){
+        List<UserResponse> users = userService.getUsers();
+        assertEquals(2, users.size());
     }
 
     @Test
     void updateUser(){
+        User user = userRepository.findByUsername("testuser1");
+        assertEquals(180, user.getHeight());
 
+        UserRequest userRequest = new UserRequest(user);
+        userRequest.setHeight(300);
+
+        UserResponse editUser = userService.updateUser(userRequest, "testuser1");
+        assertEquals(300, editUser.getHeight());
+    }
+
+    @Test
+    void removeTokenFromUser(){
+        User user = userRepository.findByUsername("testuser1");
+        assertEquals(25, user.getCredits());
+
+        userService.removeOneCreditFromUser("testuser1");
+        assertEquals(24, user.getCredits());
     }
 }
